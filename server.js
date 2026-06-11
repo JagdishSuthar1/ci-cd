856f78e65886a4eacba2df98b998bb39d89ad604
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2026-06-11 11:40:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 
 dotenv.config()
 const app = express()
-
+app.get("", (req, res)=>{
+    return res.status(200).send("Hello Jagdish")
+})
 const PORT = process.env.PORT
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
