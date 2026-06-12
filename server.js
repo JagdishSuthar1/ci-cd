b211d8207f44f941d394101bfae421fd6624a82c
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2026-06-12 10:17:49 +0530

    Added ci-cd

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const app = express()
 app.get("", (req, res)=>{
     return res.status(200).send("Hello Jagdish")
 })
+
 const PORT = process.env.PORT
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
